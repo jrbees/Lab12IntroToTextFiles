cea45fe4b16b4f17eddd1d2f42ebdc49cacebc39
Jonah Bees <j-bees@onu.edu>
2024-04-25 09:49:48 -0400

    P3

--- Lab12part1.cpp
+++ Lab12part1.cpp
@@ -1,98 +1,51 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include <vector>
 
 using namespace std;
 
-
 void pressEnterToContinue();
 bool readFromFile(string filename);
-bool readFromCSVFile(string filename);
-
-
 
 int main() {
-	string datafile;
-	cout << "Lab 12 Intro to Text Files!\n";
-	cout << "Enter Filename: ";
-	getline(cin, datafile);
-	bool status = readFromCSVFile(datafile);
-	if (status) cout << "success!\n";
-	else		cout << "failture. ;-(\n";
+    string datafile;
+    cout << "Lab 12 Intro to Text Files!\n";
+    cout << "Enter Filename: ";
+    getline(cin, datafile);
+    bool status = readFromFile(datafile);
+    if (status) cout << "Success!\n";
+    else        cout << "Failure. ;-(\n";
 
-
-	pressEnterToContinue();
+    pressEnterToContinue();
 }
 
-
-
-
 void pressEnterToContinue() {
-	cout << "\nPress enter to continue... \n";
-	cin.clear();
-	while (cin.get() != '\n') continue;
+    cout << "\nPress enter to continue... \n";
+    cin.clear();
+    while (cin.get() != '\n') continue;
 }
 
 bool readFromFile(string filename) {
-	ifstream inFile;
-	string movieTitle;
-	int		year;
-	inFile.open(filename);
+    ifstream inFile;
+    string word;
+    int wordCount = 0;
+    int charCount = 0;
 
-	if (inFile.fail()) {
-		cout << "read error - sorry/n";
-		return false;
-	}
+    inFile.open(filename);
+    if (inFile.fail()) {
+        cout << "Read error - sorry\n";
+        return false;
+    }
 
-	while (true) {
-		inFile >> movieTitle >> year;
-		if(inFile.fail()) break;
-		cout << movieTitle << "; " << year << endl;
-	}
+    while (inFile >> word) {
+        wordCount++;
+        charCount += word.size(); // Add the size of the current word to charCount
+    }
 
-	inFile.close();
-	return true;
-}
+    cout << "Word count: " << wordCount << endl;
+    cout << "Character count: " << charCount << endl;
 
-bool readFromCSVFile(string filename) {
-	ifstream inFile;
-	istringstream thisLine;
-	string movieInfo;
-	string movieTitle;
-	string year;
-	string director;
-
-	inFile.open(filename);
-	if (inFile.fail()) {
-		cout << "read error - sorry\n";
-		return false;
-	}
-
-
-
-	while (true) {
-		getline(inFile, movieInfo);
-		if (inFile.fail()) break;
-		thisLine.clear();
-		thisLine.str(movieInfo);
-		getline(thisLine, movieTitle, ',');
-		getline(thisLine, year, ',');
-
-		vector<string> directorList;
-		string dir;
-		while (getline(thisLine, dir, ','))
-			directorList.push_back(dir);
-			
-		director = directorList[0];
-		for (int i = 1; i < directorList.size(); i++) {
-			director = director + " and " + directorList[i];
-		}
-		cout << movieTitle << "is a " << year << " film directed by " << director << endl;
-
-
-	}
-	inFile.close();
-	return true;
+    inFile.close();
+    return true;
 }
